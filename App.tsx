8e79edf0324de57a7064ffc4928f621bcc2b1d31
Jon Stuebe <jstuebe@gmail.com>
2022-03-21 10:49:08 -0700

    chore: prepping for build

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { LogBox } from "react-native";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
 import "./firebase";
+
+LogBox.ignoreLogs(["AsyncStorage has been extracted"]); // Ignore log notification by message
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
